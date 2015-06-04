d43706b767503a7bb5c2c8e6967e04c521d02558
ramzapl <pryiwumek@interia.pl>
2015-06-04 20:36:00 +0200

    add

---
+++ src/LiczbyPierwsze.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+
+public class LiczbyPierwsze {
+	
+	static int iloscLiczb;
+	static Scanner scan;
+	static int[] tablicaLiczb;
+	static int licznik = 0;
+	
+	public static void main(String[] args) {
+		System.out.println("Podaj ilosc liczb do sprwdzenia:");
+		scan = new Scanner(System.in);
+		iloscLiczb = scan.nextInt();
+		
+		while (iloscLiczb < 1)
+		{
+			iloscLiczb = scan.nextInt();
+		}
+		
+		System.out.println("Podaj kolejne liczby:");
+		tablicaLiczb = new int[iloscLiczb];
+		
+		for(int i = 0; i < iloscLiczb; i++)
+		{
+			tablicaLiczb[i] = scan.nextInt();
+		}
+		
+		System.out.println("Liczby pierwsze: to");
+		
+		for(int i = 0; iloscLiczb > i; i++)
+		{
+			for (int j = 1; j < tablicaLiczb[i]; j++)
+			{
+				if (tablicaLiczb[i] % j == 0)
+				{		
+					licznik ++;	
+				}
+			}
+			
+			if (licznik == 1)
+			{
+				System.out.println(tablicaLiczb[i]);
+				
+			}
+			licznik = 0;
+		}
+		
+		
+	}
+
+}
